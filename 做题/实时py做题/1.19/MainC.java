7e44c95e585e504b7c63192513a66be370c36144
SilveryZz <2358066404@qq.com>
2024-01-20 10:09:30 +0800

    continue

--- 做题/实时py做题/1.19/MainC.java
+++ 做题/实时py做题/1.19/MainC.java
@@ -17,36 +17,39 @@ public class MainC{
    
      
     static void solv(ac io) {
-        ac in=io;
-        int n=in.nextInt();
-        int m=in.nextInt();
-        System.out.println(m+"  "+n);
-        int arr[]=new int[n+1];
-        for(int i=1;i<=n;i++)arr[i]=in.nextInt();
-        int f=0;
-        int clo[]=new int[n+1];
-        clo[0]=-1;
-        for(int i=2;i<=n;i++){
-            if(arr[i]!=arr[i-1]){
-                clo[i]=f++;
-            }else{
-                clo[i]=f;
+        ac in = io;
+        int n = in.nextInt();
+        int m = in.nextInt();
+        System.out.println(m + "  " + n);
+        int arr[] = new int[n + 1];
+        for (int i = 1; i <= n; i++)
+            arr[i] = in.nextInt();
+        int f = 0;
+        int clo[] = new int[n + 1];
+        clo[0] = -1;
+        for (int i = 2; i <= n; i++) {
+            if (arr[i] != arr[i - 1]) {
+                clo[i] = f++;
+            } else {
+                clo[i] = f;
             }
         }
-        int[]pre=new int[n+1];
-        pre[1]=1;
-        for(int i=2;i<=n;i++){
-            pre[i]=pre[i-1];
-            if(clo[i]!=clo[i-1])pre[i]++;
+        int[] pre = new int[n + 1];
+        pre[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            pre[i] = pre[i - 1];
+            if (clo[i] != clo[i - 1])
+                pre[i]++;
 
         }
-        while(m-->0){
-            int a=in.nextInt(),b=in.nextInt();
-            int res=pre[b]-pre[a];
-            if(clo[a]==clo[a-1])res++;
+        while (m-- > 0) {
+            int a = in.nextInt(), b = in.nextInt();
+            int res = pre[b] - pre[a];
+            if (clo[a] == clo[a - 1])
+                res++;
             System.out.println(res);
         }
-        
+
     }
 
  
